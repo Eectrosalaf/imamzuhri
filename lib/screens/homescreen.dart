d7cbdd7ee7fb66904533e10d7ece32d34acb0fed
eectrosalaf <electroflutter@gmail.com>
2025-01-27 11:38:16 +0100

    profile screen design

--- lib/screens/homescreen.dart
+++ lib/screens/homescreen.dart
@@ -1,6 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-
+import 'package:imamzuhri/utils/constants.dart';
 
 class Homescreen extends StatefulWidget {
   const Homescreen({super.key});
@@ -14,14 +14,79 @@ class _HomescreenState extends State<Homescreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: ElevatedButton(
-            onPressed: () {
-              FirebaseAuth.instance.signOut();
-              //return const LoginScreen();
-               Navigator.pushNamed(context, '/loginscreen');
-            },
-            child: const Text('sign out')),
+      body: Column(
+        children: [
+          Container(
+            decoration: const BoxDecoration(
+                color: DesignColors.secondaryColor,
+                borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(30),
+                    bottomRight: Radius.circular(30))),
+            height: 400,
+            width: double.infinity,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                const Text(
+                  'Profile',
+                  style: TextStyle(
+                    color: DesignColors.activeTextColor,
+                    fontSize: 24,
+                    fontStyle: FontStyle.normal,
+                    fontWeight: FontWeight.normal,
+                  ),
+                ),
+                const SizedBox(
+                  height: 70,
+                ),
+                CircleAvatar(
+                  radius: 50,
+                  child: Image.asset(
+                    'images/zurilogo.png',
+                  ),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                const Text(
+                  'Olasunkanmi Quadri Abiodun',
+                  style: TextStyle(
+                    color: DesignColors.activeTextColor,
+                    fontSize: 24,
+                    fontStyle: FontStyle.normal,
+                    fontWeight: FontWeight.normal,
+                  ),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                const Text(
+                  'Tutor and Manager',
+                  style: TextStyle(
+                    color: DesignColors.activeTextColor,
+                    fontSize: 20,
+                    fontStyle: FontStyle.italic,
+                    fontWeight: FontWeight.normal,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+      backgroundColor: DesignColors.primaryColor,
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: DesignColors.secondaryColor ,
+        onPressed: () {
+          FirebaseAuth.instance.signOut();
+
+          Navigator.pushNamed(context, '/loginscreen');
+        },
+        child: const Icon(
+          Icons.logout_rounded,
+          color: DesignColors.activeTextColor,
+          size: 30,
+        ),
       ),
     );
   }
